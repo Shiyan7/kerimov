fb0c905527996804b7643c704be8aec05366f3a9
Jeka Shiyan <ncringe31@gmail.com>
2022-09-06 15:56:59 +0300

    some fixes

--- src/js/components/fullpage.js
+++ src/js/components/fullpage.js
@@ -3,5 +3,6 @@ import fullpage from 'fullpage.js'
 document.addEventListener('DOMContentLoaded', () => {
   new fullpage('#fullpage', {
 	  slideSelector: '.fullpage-slide',
+    normalScrollElements: '.menu'
   })
 });
