49af2bd80f7209c664d2a58a27539424b0709b3e
Jeka Shiyan <ncringe31@gmail.com>
2022-09-07 17:12:05 +0300

    fixed header

--- src/js/components/fullpage.js
+++ src/js/components/fullpage.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const scrollToTop = document.querySelector('.footer__scroll-top')
 
   const fullpage = new Fullpage('#fullpage', {
+    fixedElements: '.header',
 	  slideSelector: '.fullpage-slide',
     normalScrollElements: '.menu',
   })
