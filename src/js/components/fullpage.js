05b2665dbd0f0f458d349f32b22ff0314c1dbf90
Jeka Shiyan <ncringe31@gmail.com>
2022-09-09 16:50:18 +0300

    some fixes

--- src/js/components/fullpage.js
+++ src/js/components/fullpage.js
@@ -3,11 +3,19 @@ import Fullpage from 'fullpage.js'
 document.addEventListener('DOMContentLoaded', () => {
 
   const scrollToTop = document.querySelector('.footer__scroll-top')
+  const header = document.querySelector('.header')
 
   const fullpage = new Fullpage('#fullpage', {
-    fixedElements: '.header',
 	  slideSelector: '.fullpage-slide',
     normalScrollElements: '.menu',
+
+    onLeave: function(section, slide, position, direction){
+      if(slide.isLast) {
+        header.classList.add('header--hidden')
+      } else {
+        header.classList.remove('header--hidden')
+      }
+    },
   })
 
   scrollToTop?.addEventListener('click', () => {
