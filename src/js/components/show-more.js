db5f62374f41e57ee67b34437d2cbf7eef59d0be
Jeka Shiyan <ncringe31@gmail.com>
2022-09-20 17:34:54 +0300

    some fixes

--- src/js/components/show-more.js
+++ src/js/components/show-more.js
@@ -1,15 +1,25 @@
+import $ from 'jquery'
+
 document.addEventListener('DOMContentLoaded', () => {
   const showMoreWrappers = document.querySelectorAll('[data-show-more]');
 
   showMoreWrappers?.forEach(el => {
     const btn = el.querySelector('[data-show-button]');
     const content = el.querySelector('[data-show-content]')
+    const wrapper = $('.about-text__wrapper')
     const title = el.querySelector('[data-show-title]')
 
     btn?.addEventListener('click', () => {
+
       title?.classList.toggle('active')
       content?.classList.toggle('active')
       btn?.classList.toggle('active')
+
+      if(content.classList.contains('active')) {
+        wrapper.slideDown(300)
+      } else {
+        wrapper.slideUp(300)
+      }
     })
   })
 })
